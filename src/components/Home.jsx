61c1a5060f94078fec7af39bd2b24306b0914146
rakib8680 <rakib.cst.1st@gmail.com>
2023-04-10 16:50:48 +0600

    categories data loaded

--- src/components/Home.jsx
+++ src/components/Home.jsx
@@ -1,9 +1,21 @@
-import React from 'react';
+import React, { useEffect, useState } from 'react';
+import Category from './Category';
 
 const Home = () => {
+
+    const [categories, setCategories] = useState([])
+
+    useEffect(() => {
+        fetch('categories.json')
+            .then(res => res.json())
+            .then(categoriesData => setCategories(categoriesData.categories))
+    }, [])
+
+
     return (
-        <div>
-            <div className='bg-gradient-to-r from-[#eef0fc] to-[#f1ecff]'>
+        <>
+            {/* banner  */}
+            <div className='bg-gradient-to-r from-[#f2f3fb] to-[#f7f4ff]'>
                 <div className='md:flex justify-center container mx-auto items-center px-4 md:p-0'>
                     <div className='md:w-[420px] space-y-7'>
                         <h1 className='md:text-6xl text-3xl font-bold md:leading-snug'><span className='pb-5 '>One Step</span> Closer To Your <span className='text-gradient'>Dream Job</span>
@@ -16,7 +28,21 @@ const Home = () => {
                     </div>
                 </div>
             </div>
-        </div>
+
+
+            {/* category section  */}
+            <section className='my-32 container mx-auto'>
+                <div className='text-center mb-5'>
+                    <h1 className='text-4xl font-semibold'>Job Category List</h1>
+                    <p className='text-sm  text-slate-500 mt-5'>Explore thousands of job opportunities with all the information you need. Its your future</p>
+                    <div className='flex justify-center gap-6 mt-10'>
+                        {
+                            categories.map(category => <Category category={category} key={category.id}></Category>)
+                        }
+                    </div>
+                </div>
+            </section>
+        </>
     );
 };
 
