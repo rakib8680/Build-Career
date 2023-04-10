b61baf0995aa67986fe14c0a33cc666c9028da38
rakib8680 <rakib.cst.1st@gmail.com>
2023-04-10 12:44:51 +0600

    first commit

---
+++ src/main.jsx
@@ -0,0 +1,22 @@
+import React from 'react'
+import ReactDOM from 'react-dom/client'
+import { createBrowserRouter, RouterProvider } from 'react-router-dom'
+import App from './App'
+import Home from './components/Home'
+import './index.css'
+
+
+const router = createBrowserRouter([
+  {
+    path: '/',
+    element: <App></App>,
+    children: [
+      {
+        path: '/',
+        element: <Home></Home>
+      }
+    ]
+  }
+])
+
+ReactDOM.createRoot(document.getElementById('root')).render(<RouterProvider router={router}></RouterProvider>)
