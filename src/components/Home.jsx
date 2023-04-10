52e9f39e0515077eaa1a5fe716215f9b240d0f14
rakib8680 <rakib.cst.1st@gmail.com>
2023-04-10 16:06:23 +0600

    banner section done

--- src/components/Home.jsx
+++ src/components/Home.jsx
@@ -4,12 +4,15 @@ const Home = () => {
     return (
         <div>
             <div className='bg-gradient-to-r from-[#eef0fc] to-[#f1ecff]'>
-                <div className='flex container mx-auto p-4'>
-                    <div>
-                        <h1 className='md:text-6xl text-3xl'>One Step Closer To Your Dream Job</h1>
+                <div className='md:flex justify-center container mx-auto items-center px-4 md:p-0'>
+                    <div className='md:w-[420px] space-y-7'>
+                        <h1 className='md:text-6xl text-3xl font-bold md:leading-snug'><span className='pb-5 '>One Step</span> Closer To Your <span className='text-gradient'>Dream Job</span>
+                        </h1>
+                        <p className='text-slate-600 text-sm md:text-base leading-7'>Explore thousands of job opportunities with all the information you need. Its your future. Come find it. Manage all your job application from start to finish.</p>
+                        <button className='my-btn-primary'>Get Started</button>
                     </div>
                     <div>
-                        <img className='md:w-[700px] md:h-[600px]' src="../../public/All Images/men.png" />
+                        <img className='md:w-[700px] md:h-[630px]' src="../../public/All Images/men.png" />
                     </div>
                 </div>
             </div>
